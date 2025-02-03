4e57f85765f86ed75a49ce3bc3f5b385f5fa2ad4
Anh Dao <duy123456dth@gmail.com>
2025-02-02 17:42:13 -0800

    Completed homework assignment

---
+++ cmake-build-debug/CMakeFiles/CppBootcamp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppBootcamp.dir/main.cpp.o"
+  "CMakeFiles/CppBootcamp.dir/main.cpp.o.d"
+  "CppBootcamp"
+  "CppBootcamp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppBootcamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
